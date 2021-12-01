6838bc02b746a270998ba1e4f09401f88ac34d1a
Iván Rodríguez García <i.rodriguezg.2018@alumnos.urjc.es>
2021-12-01 17:31:48 +0100

    Health System
    Se ha creado un sistema de vidas y un controlador de nivel.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,39 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    //Singletone
+    public static GameManager instance;
+
+    //Editor
+    public GameObject player;
+    public Transform playerSpawn;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        instance = this;
+    }
+
+    private void Start()
+    {
+        DoNewBall();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //Mechanics
+    public void DoGameOver()
+    {
+        Debug.Log("Te has quedado sin vidas, fin del juego.");
+    }
+
+    public void DoNewBall(GameObject previousBall = null)
+    {
+        if (previousBall) Destroy(previousBall);
+        Instantiate(player, playerSpawn.position, Quaternion.identity);
     }
 }
