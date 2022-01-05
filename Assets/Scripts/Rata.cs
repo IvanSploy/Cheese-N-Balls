ffa30c59bd7a59cbf58b65cf6f98aeb177486849
jvicog <javiervico8@gmail.com>
2022-01-05 15:11:53 +0100

    comportamiento rata

--- Assets/Scripts/Rata.cs
+++ Assets/Scripts/Rata.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Rata : MonoBehaviour
 {
     public GameObject [] posiciones = new GameObject [10];
+    public RataPositions rataPositions;
     private Animator anim;
     private Collider2D collider;
     private int randomNumber;
@@ -12,7 +13,7 @@ public class Rata : MonoBehaviour
     public float speed;
     private float step;
     private Vector3 nextPos;
-    private GameObject posFinal;
+    private int pos;
     // Start is called before the first frame update
 
     private void Awake()
@@ -23,11 +24,11 @@ public class Rata : MonoBehaviour
     void Start()
     {
         collider = GetComponent<Collider2D>();
-        posFinal = FindObjectOfType<DamageCheeseBehaviour>().gameObject;
-
-        //
-        // A QUE POSICION VA AL EMPEZAR (VARIAS POSIBLES) -- nextPos = posiciones[random entre algunas]
-        //
+        rataPositions = FindObjectOfType<RataPositions>();
+        posiciones = rataPositions.posiciones;
+        pos = Random.Range(0, 2);
+        nextPos = new Vector3(posiciones[pos].transform.position.x, posiciones[pos].transform.position.y + 0.6f, posiciones[pos].transform.position.z);
+        SetAnimation(true);
     }
 
     // Update is called once per frame
@@ -35,15 +36,23 @@ public class Rata : MonoBehaviour
     {
         if(canMove)
         {
-            Quaternion rotTarget = Quaternion.LookRotation(nextPos - this.transform.position);
+            Quaternion rotTarget = Quaternion.LookRotation(Vector3.forward, Quaternion.Euler(0, 0, 180) * (nextPos - this.transform.position));
             this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, rotTarget, 250f * Time.deltaTime);
             step = speed * Time.deltaTime;
             transform.position = Vector3.MoveTowards(transform.position, nextPos, step);
+            if(nextPos == transform.position)
+            {
+                canMove = false;
+                StartCoroutine(TiempoAleatorio());
+                SetAnimation(false);
+            }
         }
-        if(nextPos == transform.position)
+        else
         {
-            StartCoroutine(TiempoAleatorio());
+            Quaternion rotTarget = Quaternion.LookRotation(Vector3.forward, Quaternion.Euler(0, 0, 180) * (posiciones[8].transform.position - this.transform.position));
+            this.transform.rotation = Quaternion.RotateTowards(this.transform.rotation, rotTarget, 250f * Time.deltaTime);
         }
+
     }
 
     //USAR ESTE METODO PARA CAMBIAR LA ANIMACION DE LA RATA.
@@ -55,45 +64,45 @@ public class Rata : MonoBehaviour
 
     IEnumerator TiempoAleatorio()
     {
-        randomNumber = Random.Range(2, 4);
+        randomNumber = Random.Range(4, 7);
         yield return new WaitForSeconds(randomNumber);
+        newPos();
+        if (pos != 8)
+            nextPos = new Vector3(posiciones[pos].transform.position.x, posiciones[pos].transform.position.y + 0.6f, posiciones[pos].transform.position.z);
+        else
+            nextPos = new Vector3(posiciones[pos].transform.position.x, posiciones[pos].transform.position.y, posiciones[pos].transform.position.z);
+        SetAnimation(true);
         canMove = true;
-        StartCoroutine(TiempoAleatorio());
-
-        //
-        // EL METODO DE ABAJO
-        //
-
     }
 
-    //////////////////////////////////////////////////////////
+    ////////////////////////////////////////////////////////// 0,1 --- 2 --- 3,4 ----5 ---- 6,7 ---- 8
     //   NECESITO SABER EL MAPA PARA PODER HACER ESTO       //
     //   SIN MORIR EN EL INTENTO. PARA SABER QUE PUNTOS     //
     //   PONER Y HACER EL ALGORITMO.                        //
     //////////////////////////////////////////////////////////
 
-    /*public int[,] newPos(int pos)
+    public void newPos()
     {
         switch(pos)
         {
             case 0:
             case 1:
+                pos = Random.Range(2, 5);
+                break;
             case 2:
+                pos = Random.Range(3, 5);
+                break;
             case 3:
-                if (pos + 1 <= posiciones.GetLength(0) - 1)
-                    pos = Random.Range(pos, pos + 1);
+            case 4:
+                pos = Random.Range(5, 8);
+                break;
+            case 5:
+                pos = Random.Range(6, 8);
+                break;
+            case 6:
+            case 7:
+                pos = 8;
                 break;
         }
-        if (pos + 1 <= posiciones.GetLength(0) - 1)
-            pos = Random.Range(pos, pos + 1);
-        if (number - 1 < 0 && number + 1 > posiciones.GetLength(1) - 1)
-            return(posiciones[pos, number].transform.position);
-        else if (number - 1 < 0)
-            number = Random.Range(number, number + 1);
-        else if (number + 1 > posiciones.GetLength(1) - 1)
-            number = Random.Range(number, number + 1);
-        else
-            number = Random.Range(number - 1, number + 1);
-        //return([pos, number]);
-    }*/
+    }
 }
---
+++ Assets/Scripts/RataPositions.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RataPositions : MonoBehaviour
+{
+    public GameObject [] posiciones = new GameObject [10];
+}
